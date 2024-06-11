e310f3cb3526861cc4253e6639461efb82065e76
joeywensveen <131982352+joeywensveen@users.noreply.github.com>
2024-06-11 08:43:04 +0200

    routing added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const cors = require("cors")
 const mysql = require("mysql")
-
+const { useParams } = require("react-router-dom")
+const { useRef } = require("react")
+const bcrypt = require("bcrypt")
+const saltRounds = 10
 
 const app = express()
 app.use(express.json())
@@ -33,6 +36,31 @@ app.get("/:trainer", (req, res) => {
         const result = data
         res.json(result)
     })
+})
+
+app.put("/register", (req, res) => {
+
+    const email = req.body.gebruikersnaam
+    const newPassword = req.body.wachtwoord
+    console.log(email, newPassword)
+    const query = `UPDATE gebruikers SET wachtwoord = ? WHERE email = ?`
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(newPassword, salt, function (err, hash) {
+            db.query(query, [hash, email], (err, result) => {
+                if (err) {
+                    // Handle the error
+                    res.status(500).send("Error updating password");
+                } else {
+                    // Send a success response
+                    res.send("Password updated successfully");
+                }
+            })
+
+        });
+    });
+
+
 })
 
 app.listen(3000, () => console.log("listening"))
